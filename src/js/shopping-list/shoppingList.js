2295921472fc55820362ff5a100675ed93e673e2
Ilona Kamash <ilonka.kamash@gmail.com>
2023-05-15 13:49:37 +0200

    create pagination

--- src/js/shopping-list/shoppingList.js
+++ src/js/shopping-list/shoppingList.js
@@ -1,1 +1,31 @@
+
 import '../side-bar/supportCreateList';
+
+
+
+const listBtn = document.querySelectorAll('.sh-list__pagination');
+const page = document.querySelectorAll('.nullInList');
+console.log(page)
+
+
+
+
+
+localStorage.setItem("ui-theme", '{"name":"Mango","age":3,"isHappy":true}');
+const local = localStorage.getItem("ui-theme")
+console.log(local)
+
+function controllInLocalStorage(){
+    const savedSettings = JSON.parse(localStorage.getItem("ui-theme"));
+    console.log(savedSettings)
+    if(!savedSettings){
+        createDefaultPage()
+    }
+}
+controllInLocalStorage()
+
+
+function createDefaultPage() {
+
+    page.insertAdjacentHTML("beforeend", "<p>Ilona</p>")
+}
