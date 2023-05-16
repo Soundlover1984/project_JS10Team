9bd2dcf1598c113e204d698814e1c347f70c9ab8
Iryna <78929283+IrynaYrrr@users.noreply.github.com>
2023-05-16 20:00:18 +0300

    add style for nav link in header

--- src/js/header/header.js
+++ src/js/header/header.js
@@ -3,21 +3,19 @@ const openNav = document.querySelector('.header-menu button');
 const closeNav = document.querySelector('.close-nav button');
 const HeaderNavbar = document.querySelector('.header-navbar');
 
+
+if(window.location.pathname === "/index.html") {
+  document.querySelector('.header-shopping').classList.remove('header-active');
+  document.querySelector('.header-home').classList.add('header-active');
+} else if(window.location.pathname === "/shopping-list.html") {
+  document.querySelector('.header-home').classList.remove('header-active');
+  document.querySelector('.header-shopping').classList.add('header-active');
+}
+
+
 function showHide() {
   HeaderNavbar.classList.toggle('show');
 }
 
 openNav.onclick = showHide;
 closeNav.onclick = showHide;
-
-const headerList = document.querySelector('.header-link');
-const active = headerList.querySelector('.header-current');
-
-for (let i = 0; i < active.length; i++) {
-  console.log('123');
-  active[i].addEventListener("click", function() {
-    let current = document.querySelector('active');
-    current[0].className = current[0].className.replace("active", "");
-    this.className += " active";
-  });
-}
