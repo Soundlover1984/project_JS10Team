e45fdbbe2375b6eaf9e3393323b4fc24a74655e0
Iryna <78929283+IrynaYrrr@users.noreply.github.com>
2023-05-16 16:26:42 +0300

    refactor styles for header, add link on shopping list

--- src/js/header/header.js
+++ src/js/header/header.js
@@ -1,11 +1,22 @@
 const theBody = document.querySelector('body');
-const openNav = document.querySelector('.menu-bar button');
+const openNav = document.querySelector('.header-menu button');
 const closeNav = document.querySelector('.close-nav button');
-const Navbar = document.querySelector('.navbar');
+const HeaderNavbar = document.querySelector('.header-navbar');
 
 function showHide() {
-    Navbar.classList.toggle('show');
+  HeaderNavbar.classList.toggle('show');
 }
 
 openNav.onclick = showHide;
 closeNav.onclick = showHide;
+
+const headerList = document.querySelector("header-list");
+const active = headerList.querySelector("header-current");
+
+for (let i = 0; i < active.length; i++) {
+  active[i].addEventListener("click", function() {
+    let current = document.querySelector("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
