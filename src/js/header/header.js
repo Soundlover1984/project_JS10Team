dbd2101d678bf739dbedf859a7a229662d225566
Iryna <78929283+IrynaYrrr@users.noreply.github.com>
2023-05-16 19:31:12 +0300

    update

--- src/js/header/header.js
+++ src/js/header/header.js
@@ -10,13 +10,14 @@ function showHide() {
 openNav.onclick = showHide;
 closeNav.onclick = showHide;
 
-const headerList = document.querySelector("header-list");
-const active = headerList.querySelector("header-current");
+const headerList = document.querySelector('.header-link');
+const active = headerList.querySelector('.header-current');
 
 for (let i = 0; i < active.length; i++) {
+  console.log('123');
   active[i].addEventListener("click", function() {
-    let current = document.querySelector("active");
-    current[0].className = current[0].className.replace(" active", "");
+    let current = document.querySelector('active');
+    current[0].className = current[0].className.replace("active", "");
     this.className += " active";
   });
 }
