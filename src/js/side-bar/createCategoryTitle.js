b3a42ef15c8d2c3cd8f09672ad3a7ae7a5cd8f95
Тетяна Пономарчук <tanya061211@gmail.com>
2023-05-18 11:42:09 +0200

    update

--- src/js/side-bar/createCategoryTitle.js
+++ src/js/side-bar/createCategoryTitle.js
@@ -4,10 +4,11 @@ export { drawCategoryTitle };
 const booksApiService = new BooksApiService();
 const categoryTitle = document.querySelector('.home-collection__title');
 
-async function drawCategoryTitle(categoryName) {
+async function drawCategoryTitle(name) {
   try {
-    await booksApiService.getCategoryBooks(categoryName);
-    title = categoryName.split(' ');
+    booksApiService.selectedCategory = name;
+    await booksApiService.getCategoryBooks(name);
+    const title = name.split(' ');
     const titleLast = title.pop();
     const titleFirst = title.join(' ');
     categoryTitle.innerHTML = `${titleFirst}<span class="home-collection__title--last-word" style = "color: #4f2ee8"> ${titleLast}</span>`;
