b1420048e7a7335ad4a4d7ce2fcdfbd252dc370d
Iryna <78929283+IrynaYrrr@users.noreply.github.com>
2023-05-18 20:05:34 +0300

    add new toggle

--- src/js/header/header.js
+++ src/js/header/header.js
@@ -1,19 +1,5 @@
-const switchBtn = document.querySelector('.header__switch-btn');
-const switchOn = document.querySelector('.header__switch-on');
-const headerContainer = document.querySelector('.header__container');
-
-const headerLogo = document.querySelector('.header__logo');
-const burgerSvg = document.querySelector('.header__svg-light');
-
-const headerHome = document.querySelector('.header-home');
-const headerShopping = document.querySelector('.header-shopping');
-const shoppingSvg = document.querySelector('.header-shopping');
-
-
 const burgerBtn = document.querySelector('.header__burger-btn');
 const closeBtn = document.querySelector('.header__close-btn');
-const pageWrapper = document.querySelector('.page-wrapper');
-const categoriesList = document.querySelector('.categories__list');
 
 if(window.location.pathname === "/index.html" || window.location.pathname === "/project_js10team/index.html") {
   document.querySelector('.header-home').classList.add('header-active');
@@ -24,52 +10,6 @@ if(window.location.pathname === "/index.html" || window.location.pathname === "/
 } else {
   document.querySelector('.header-home').classList.add('header-active');
 }
-
-switchBtn.addEventListener('click', onSwitchClick);
-
-function onSwitchClick() {
-
-  if(!switchBtn.classList.contains('header__switch-on')){
-    pageWrapper.style.backgroundColor = "black";
-    categoriesList.childNodes.forEach((item) => {
-      try {
-        item.style.color = "white";
-      }
-      catch (err) {}
-    });
-    this.classList.add('header__switch-on');
-    headerContainer.classList.add('header__dark-theme');
-    burgerSvg.classList.add('header__burderSvg-dark');
-    closeBtn.classList.add('header__closeSvg-dark');
-    headerHome.style.color = "white";
-    headerShopping.style.color = "white";
-    shoppingSvg.classList.add('header__closeSvg-dark');
-  } else {
-    pageWrapper.style.backgroundColor = "transparent";
-    categoriesList.childNodes.forEach((item) => {
-      try {
-        item.style.color = "#1119";
-      }
-      catch (err) {}
-    });
-    this.classList.remove('header__switch-on');
-    headerContainer.classList.remove('header__dark-theme');
-    burgerSvg.classList.remove('header__burderSvg-dark');
-    closeBtn.classList.remove('header__closeSvg-dark');
-    headerHome.style.color = "black";
-    headerShopping.style.color = "black";
-    shoppingSvg.classList.remove('header__closeSvg-dark');
-  }
-
-  if (headerLogo.innerHTML.includes('svg#icon-logo--dark')){
-    headerLogo.innerHTML = headerLogo.innerHTML.replace('svg#icon-logo--dark', 'svg#icon-logo');
-  }
-  else {
-    headerLogo.innerHTML = headerLogo.innerHTML.replace('svg#icon-logo', 'svg#icon-logo--dark');
-  }
-
-}
-
 
 burgerBtn.addEventListener('click', onClickBurder);
 
