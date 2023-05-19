388090b2cd887bf6000c832b031b3fa5d1d72f9b
Тетяна Пономарчук <tanya061211@gmail.com>
2023-05-19 17:56:31 +0200

    last fix

--- src/js/header/modal-menu.js
+++ src/js/header/modal-menu.js
@@ -2,7 +2,6 @@
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
-  let isMenuOpen = false;
 
   const toggleMenu = () => {
     const isMenuOpen =
@@ -10,24 +9,23 @@
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
     mobileMenu.classList.remove('visually-hidden');
-    if (isMenuOpen) {
-      document.body.style.overflow = 'hidden';
-      document.body.style.height = '100vh';
-    } else {
-      document.body.style.overflow = '';
-      document.body.style.height = '';
-    }
+
+    const scrollLockMethod = !isMenuOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
   };
 
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
 
-  // Закрыть мобильное меню при изменении ориентации устройства на широких экранах
+  // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
     if (!e.matches) return;
     mobileMenu.classList.remove('is-open');
-    openMenuBtn.setAttribute('aria-expanded', false);
     mobileMenu.classList.add('visually-hidden');
+    openMenuBtn.setAttribute('aria-expanded', false);
+
     bodyScrollLock.enableBodyScroll(document.body);
   });
 })();
